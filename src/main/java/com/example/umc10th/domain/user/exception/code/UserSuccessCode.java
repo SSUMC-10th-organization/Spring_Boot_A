5927b4042db4db6e0f413ebbf5b34f2c4ff4550c
chaerishme <sbk93@naver.com>
2026-04-13 19:03:25 +0900

    mission/#20 - 성공 코드 수정

--- src/main/java/com/example/umc10th/domain/user/exception/code/UserSuccessCode.java
+++ src/main/java/com/example/umc10th/domain/user/exception/code/UserSuccessCode.java
@@ -9,12 +9,12 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserSuccessCode implements BaseSuccessCode {
 
-    USER_INFO_OK(HttpStatus.OK, "USER_2000", "사용자 정보 조회 성공"),
-    USER_MISSION_PROGRESS_OK(HttpStatus.OK, "USER_2001", "미션 진행 현황 조회 성공"),
-    USER_MISSIONS_OK(HttpStatus.OK, "USER_2002", "미션 목록 조회 성공"),
-    USER_MISSION_CHALLENGE_OK(HttpStatus.CREATED, "USER_2003", "미션 도전 성공"),
-    USER_MISSION_SUCCESS_OK(HttpStatus.OK, "USER_2004", "미션 성공 처리 완료"),
-    USER_JOIN_OK(HttpStatus.CREATED, "USER_2005", "회원가입 성공");
+    USER_INFO_OK(HttpStatus.OK, "200_0", "사용자 정보 조회 성공"),
+    USER_MISSION_PROGRESS_OK(HttpStatus.OK, "200_1", "미션 진행 현황 조회 성공"),
+    USER_MISSIONS_OK(HttpStatus.OK, "200_2", "미션 목록 조회 성공"),
+    USER_MISSION_CHALLENGE_CREATED(HttpStatus.CREATED, "200_3", "미션 도전 성공"),
+    USER_MISSION_SUCCESS_OK(HttpStatus.OK, "200_4", "미션 성공 처리 완료"),
+    USER_JOIN_CREATED(HttpStatus.CREATED, "201_0", "회원가입 성공");
 
     private final HttpStatus status;
     private final String code;
