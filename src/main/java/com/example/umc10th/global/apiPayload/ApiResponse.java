49e81fb57a35501ba1bdd644d849ffdc6f36bb03
chaerishme <sbk93@naver.com>
2026-04-13 16:01:04 +0900

    mission/#20 - 응답 통일 객체, 에러 핸들링할 객체 생성

---
+++ src/main/java/com/example/umc10th/global/apiPayload/ApiResponse.java
@@ -0,0 +1,39 @@
+package com.example.umc10th.global.apiPayload;
+
+import com.example.umc10th.global.apiPayload.code.BaseErrorCode;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class ApiResponse<T> {
+
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+
+    @JsonProperty("code")
+    private final String code;
+
+    @JsonProperty("message")
+    private final String message;
+
+    @JsonProperty("result")
+    private final T result;
+
+
+    public static <T> ApiResponse<T> onSuccess(BaseSuccessCode successCode, T result) {
+        return new ApiResponse<>(true, successCode.getCode(), successCode.getMessage(), result);
+    }
+
+    public static <T> ApiResponse<T> onFailure(BaseErrorCode errorCode, T result) {
+        return new ApiResponse<>(false, errorCode.getCode(), errorCode.getMessage(), result);
+    }
+
+    public static ApiResponse<Void> onFailure(BaseErrorCode code) {
+        return onFailure(code, null);
+    }
+}
