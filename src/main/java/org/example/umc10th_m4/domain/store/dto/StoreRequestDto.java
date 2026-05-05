0a110ae2b1c16fb35a3d470ab933085ed08c8ad6
lclee0520 <lc_lee0520@naver.com>
2026-05-05 12:20:26 +0900

    fix/#28 PR 리뷰 반영

--- src/main/java/org/example/umc10th_m4/domain/store/dto/StoreRequestDto.java
+++ src/main/java/org/example/umc10th_m4/domain/store/dto/StoreRequestDto.java
@@ -1,9 +1,11 @@
 package org.example.umc10th_m4.domain.store.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class StoreRequestDto {
     @Getter
+    @NoArgsConstructor
     public static class UpdateStoreDto {
         private String name;
         private String location;
